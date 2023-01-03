b0bcb2efa7bc432608861830a66333af46a2089f
Justin Berger Howes <howes.j.j@gmail.com>
2023-01-03 19:26:58 +0800

    js class setup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
 window.addEventListener("load", ()=>{
+    //canvas setup
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 500;
+    canvas.height = 500;
 
+    class InputHandler{
+        constructor(){
+
+        }
+    };
+    class Projectile{
+        constructor(){
+
+        }
+    };
+    class Particle{
+        constructor(){
+
+        }
+    };
+    class Player{
+        constructor(){
+
+        }
+    };
+    class Enemy{
+        constructor(){
+
+        }
+    };
+    class Layer{
+        constructor(){
+
+        }
+    };
+    class Background{
+        constructor(){
+
+        }
+    };
+    class UI{
+        constructor(){
+
+        }
+    };
+    class Game{
+        constructor(){
+
+        }
+    };
 })

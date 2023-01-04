4ef1d9ac5d9eb971f41eefcda9dfceafef304949
Justin Berger Howes <howes.j.j@gmail.com>
2023-01-04 21:00:25 +0800

    solid base for game for starting point

--- js/script.js
+++ js/script.js
@@ -149,14 +149,19 @@ window.addEventListener("load", ()=>{
             this.color = "white";
         }
         draw(context){
+            context.save();
             context.fillStyle = this.color;
+            context.shadowOffsetX = 2;
+            context.shadowOffsetY = 2;
+            context.shadowColor = "black";
             context.font = this.fontsize + "px" + this.fontFamily;
             //score
-            context.fillText(this.game.score, 20, 40);
+            context.fillText('Score: ' + this.game.score, 20, 40);
             //ammo
             for (let i = 0; i < this.game.ammo; i++){
                 context.fillRect(20 + 5 * i, 50, 3, 20);
             }
+            context.restore();
         }
     }
 

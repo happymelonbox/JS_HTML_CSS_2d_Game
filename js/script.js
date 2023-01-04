e084bd30c07db49794ed15dd13e7d3812cc9de47
Justin Berger Howes <howes.j.j@gmail.com>
2023-01-04 14:21:10 +0800

    enemy parent class written and angler1 begun

--- js/script.js
+++ js/script.js
@@ -9,22 +9,23 @@ window.addEventListener("load", ()=>{
         constructor(game){
             this.game = game;
             window.addEventListener("keydown", e => {
-                if ((
-                    (e.key === "ArrowUp") || (e.key === "ArrowDown"))
+                if (
+                    ((e.key === "ArrowUp") || (e.key === "ArrowDown"))
                 &&
-                    this.game.keys.indexof(e.key) === -1){
+                    this.game.keys.indexOf(e.key) === -1){
                     this.game.keys.push(e.key);
                 } else if (e.key === " "){
                     this.game.player.shootTop()
                 }
             })
             window.addEventListener("keyup", e => {
-                if (this.game.keys.indexof(e.key) > -1){
-                    this.game.keys.splice(this.game.keys.indexof(e.key), 1);
+                if (this.game.keys.indexOf(e.key) > -1){
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
                 }
             })
         }
-    };
+    }
+
     class Projectile {
         constructor(game, x, y){
             this.game = game;
@@ -45,12 +46,14 @@ window.addEventListener("load", ()=>{
             context.fillStyle = "yellow";
             context.fillRect(this.x, this.y, this.width, this.height);
         }
-    };
+    }
+
     class Particle {
         constructor(){
 
         }
-    };
+    }
+
     class Player {
         constructor(game){
             this.game = game;
@@ -89,34 +92,73 @@ window.addEventListener("load", ()=>{
             if (this.game.ammo > 0){
             this.projectiles.push(new Projectile(this.game, this.x, this.y));
             this.game.ammo--;
-        }
-    };
-    class Enemy {
-        constructor(){
+            }
+        };
+    }
 
+    class Enemy {
+        constructor(game){
+            this.game = game;
+            this.x = this.game.width;
+            this.speedX = Math.random() * -1.5 - 0.5;
+            this.markedForDeletion = false;
         }
-    };
+        update(){
+            this.x += this.speedX;
+            if (this.x + this.width < 0){
+                this.markedForDeletion = true;
+            }
+        }
+        draw(){
+            context.fillStyle = "red";
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
+    class Angler1 extends Enemy{
+        constructor(game){
+            super(game);
+            this.width = 228;
+            this.height = 169;
+            this.y = Math.random() * (this.game.height * 0.9 - this.height);
+        }
+    }
+
     class Layer {
         constructor(){
 
         }
-    };
+    }
+
     class Background {
         constructor(){
 
         }
-    };
-    class UI {
-        constructor(){
+    }
 
+    class UI {
+        constructor(game){
+            this.game = game;
+            this.fontsize = 25;
+            this.fontFamily = "Helvetica";
+            this.color = "yellow";
         }
-    };
+        draw(context){
+            //ammo
+            context.fillStyle = this.color;
+            for (let i = 0; i < this.game.ammo; i++){
+                context.fillRect(20 + 5 * i, 50, 3, 20);
+            }
+        }
+    }
+
     class Game {
         constructor(width, height){
             this.width = width;
             this.height = height;
             this.player = new Player(this);
             this.input = new InputHandler(this);
+            this.ui = new UI(this);
             this.keys = [];
             this.ammo = 20;
             this.maxAmmo = 50;
@@ -129,17 +171,18 @@ window.addEventListener("load", ()=>{
             if(this.ammoTimer > this.ammoInterval){
                 if (this.ammo < this.maxAmmo){
                     this.ammo++;
-                    this.ammoTimer = 0;
-                } else {
-                    this.ammoTimer += deltaTime;
                 }
+                this.ammoTimer = 0;
+            } else {
+                this.ammoTimer += deltaTime;
             }
         }
 
         draw(context){
             this.player.draw(context);
+            this.ui.draw(context);
         }
-    };
+    }
 
     const game = new Game(canvas.width, canvas.height);
     let lastTime = 0;
